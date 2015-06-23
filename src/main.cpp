b7352adb9cc5a47b46acfd8bd7f0207a8f2e4076
Blito <pabloarubi@gmail.com>
2015-06-23 20:20:26 -0300

    #7 - Added SDL2.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <array>
 #include <iostream>
 
+// GLM includes
 #include <glm/vec3.hpp>
 
-int main()
+// SDL2 includes
+#include <SDL.h>
+
+int main(int argc, char** args)
 {
+	// GLM tests
 	std::array<int, 5> myArray;
 	for (auto i : myArray)
 	{
@@ -13,4 +18,16 @@ int main()
 
 	glm::vec3 myVec(1.0f, 2.0f, 3.0f);
 	std::cout << myVec.x << " " << myVec.y << " " << myVec.z << std::endl;
+
+	// SDL tests
+	if (SDL_Init(SDL_INIT_VIDEO) != 0){
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	else
+	{
+		std::cout << "SDL2 enabled!" << std::endl;
+	}
+	SDL_Quit();
+	return 0;
 }
