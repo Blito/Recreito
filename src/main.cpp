f70a1fed4f1600a9316a26c9841b45c8ccae6141
Blito <pabloarubi@gmail.com>
2015-07-11 15:54:23 -0300

    Basic SDL window working.

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@ int main(int argc, char *argv[])
     SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(window);
 
+    SDL_GL_SetSwapInterval(1);
+
     glewExperimental = GL_TRUE;
     glewInit();
 
@@ -23,6 +25,26 @@ int main(int argc, char *argv[])
     glGenBuffers(1, &vertexBuffer);
 
     printf("%u\n", vertexBuffer);
+
+    SDL_Event event;
+    bool quit = false;
+    while (!quit)
+    {
+        /* Check for new events */
+        while(SDL_PollEvent(&event))
+        {
+            /* If a quit event has been sent */
+            if (event.type == SDL_QUIT)
+            {
+                /* Quit the application */
+                quit = 1;
+            }
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0.0, 0.0, 0.0, 1.0);//clear red
+        SDL_GL_SwapWindow(window);
+    }
 
     SDL_GL_DeleteContext(context);
     SDL_Quit();
